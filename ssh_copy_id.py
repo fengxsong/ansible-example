c3d2599d692b4f6450a3c1d6795bf94d887c5af9
fengxsong <fengxsong@outlook.com>
2017-11-06 16:05:12 +0800

    Update ssh_copy_id.py

--- ssh_copy_id.py
+++ ssh_copy_id.py
@@ -1,29 +1,86 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+import argparse
+import getpass
+import json
+import logging
 import os
 import paramiko
 
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
 
-def ssh_copy_id_via_paramiko(host, user, pwd, p=22, timeout=8):
-    with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as f:
-        pubkey = f.read()
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+logger.addHandler(ch)
+
+RSA_PUBKEY = '~/.ssh/id_rsa.pub'
+DSA_PUBKEY = '~/.ssh/id_dsa.pub'
+
+def get_pubkey():
+    for f in map(lambda f: os.path.expanduser(f), [RSA_PUBKEY, DSA_PUBKEY]):
+        if os.path.isfile(f):
+            return f
+    return None
+
+
+def copy_pubkey(hostname, username, passwd, port, timeout=8):
+    pubkey = get_pubkey()
+    if not pubkey:
+        print "Make sure ~/.ssh/(id_rsa|id_dsa).pub exists"
+        sys.exit(1)
+    with open(pubkey) as f:
+        buf = f.read()    
+
     cmd = """
     mkdir -p $HOME/.ssh/ && echo "%s" >> $HOME/.ssh/authorized_keys && chmod 600 $HOME/.ssh/authorized_keys && chmod 700 $HOME/.ssh
-    """ % pubkey
+    """ % buf
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        client.connect(hostname=host, port=p, username=user, password=pwd, timeout=timeout)
+        client.connect(hostname=hostname, port=port, username=username, password=passwd, timeout=timeout)
         stdin, stdout, stderr = client.exec_command(cmd)
+        logger.info('Stdin', stdin)
+        logger.info('Stdout', stdout)
+        logger.info('Stderr', stderr)
     except Exception as e:
-        with open('copy_id_fail.log', 'a+') as log:
-            log.write('%s\t%s\n' % (host, e))
+        logger.warning('Hostname %s: %s'%(hostname, e))
+        return False
+    return True
+        
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hostlist", help="A hostlist file")
+    args = parser.parse_args()
+    current_user = getpass.getuser()
+
+    fn = args.hostlist if args.hostlist else "hosts"
+    perm = 'r+' if os.path.isfile(fn) else 'a+'
+    data = dict()
+    with open(fn, perm) as f:
+        try:
+            data = json.load(f)
+            if len(data) > 0:
+                for hostname, kw in data.items():
+                    if kw.get('done'):
+                        logger.info('Skip %s..', hostname)
+                        continue
+                    port = kw.get('port', '22')
+                    username = kw.get('username', current_user)
+                    passwd = kw.get('passwd', os.getenv('COMMON_PASSWD'))
+                    if copy_pubkey(hostname, username, passwd, int(port)):
+                        data[hostname]['done'] = True
+        except ValueError:
+            data['precise_hostname'] = {'port': 22, 'user': '', 'passwd': ''}
+            print """
+\033[91mInitialize hostlist file **hosts**, edit file before rerun command\033[0m"""
+        f.seek(0)
+        json.dump(data, f, indent=4)
+        f.truncate()
+            
 
 if __name__ == '__main__':
-    with open('hosts') as f:
-        for line in f.readlines():
-            if not line.startswith('#'):
-                hostname, port, username, password = line.split()
-                ssh_copy_id_via_paramiko(hostname, username, password, int(port))
+    main()
